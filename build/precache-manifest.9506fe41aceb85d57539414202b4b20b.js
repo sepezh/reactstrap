ef05d75274f39ef864d2d7eff605e4e970c202cc
sepide eslamnezhad <sepide.eslamnezhad@gmail.com>
2020-04-14 01:28:49 +0430

    build added

---
+++ build/precache-manifest.9506fe41aceb85d57539414202b4b20b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "752029ba3fb7421b61074cd626712db4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "055f2d3b0f6a8994a550",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "f65ebf2575840444d7ed",
+    "url": "/static/css/main.43323cdb.chunk.css"
+  },
+  {
+    "revision": "055f2d3b0f6a8994a550",
+    "url": "/static/js/2.c4908065.chunk.js"
+  },
+  {
+    "revision": "e306f97ee610ab850c6616952a8a0629",
+    "url": "/static/js/2.c4908065.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f65ebf2575840444d7ed",
+    "url": "/static/js/main.10083072.chunk.js"
+  },
+  {
+    "revision": "365f67d13043ab3e49cc",
+    "url": "/static/js/runtime-main.98973c29.js"
+  }
+]);
